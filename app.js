2783d12c8ec47b05891c5a739f924a77876b1954
Hayitbek <khaitbek2005@gmail.com>
2022-01-12 11:07:56 +0500

    Color flipper

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
-const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+tl = gsap.timeline();
+let hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 let btn = document.querySelector(".btn");
-let color = document.querySelector(".bg");
+let copy = document.querySelector(".copy");
+let color = document.querySelector("span.color");
 document.body.style.background = localStorage.getItem("hexColor");
-color.textContent = localStorage.getItem("hexColor")
+color.textContent = window.localStorage.getItem("hexColor");
+color.style.color = window.localStorage.getItem("hexColor");
+let pop = document.querySelector(".pop-up")
 
-btn.addEventListener("click",()=>{
-    let hexColor = '#';
-    for(let i = 0;i < 6;i++){
+btn.addEventListener("click", () => {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
         hexColor += hex[getRandomNumber()];
-        window.localStorage.setItem("hexColor",hexColor);
+        window.localStorage.setItem("hexColor", hexColor);
     }
-    document.body.style.background = localStorage.getItem("hexColor");
-    color.textContent = localStorage.getItem("hexColor")
-    if(hexColor === "#000000"){
-        btn.style.color = "#fff";
-        btn.style.borderColor = "#888888"
+    if (hexColor === "#000000") {
+        color.style.color = "#000000";
     }
+    document.body.style.background = window.localStorage.getItem("hexColor");
+    color.textContent = hexColor;
+    color.style.color = hexColor;
+});
+
+copy.addEventListener("click", () => {
+    copy_function();
+    gsap.to(".pop-up", {
+        y: 0,
+        opacity:1,
+        duration:0.5
+    })
+    setTimeout(() => {
+        gsap.to(".pop-up", {
+            y: 30,
+            opacity: 0,
+            duration: 0.5
+        })
+    },2500)
 })
 
-// get random number
-function getRandomNumber(){
+function copy_function() {
+    navigator.clipboard.writeText(window.localStorage.getItem("hexColor"));
+}
+
+
+function getRandomNumber() {
     return Math.floor(Math.random() * hex.length);
 }
